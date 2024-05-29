28647853dfe0797cf8afd23839fdbe73a0510332
RanjitaGupta123 <see.ranjita@gmail.com>
2024-05-29 16:24:28 +0000

    added E2E stage to cloud pipeline commented version test

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -16,7 +16,7 @@ test('has Jenkins in the body', async ({ page }) => {
 });
 
 
-test('has expected app version', async ({ page }) => {
+/ * test('has expected app version', async ({ page }) => {
   await page.goto('/');
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
@@ -25,4 +25,4 @@ test('has expected app version', async ({ page }) => {
 
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
   expect(isVisible).toBeTruthy();
-});
+}); */
